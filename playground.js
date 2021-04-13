8f2cc1774ed0fe36fecce90fe6a10847adf9c532
bear <bear19956011@gmail.com>
2021-04-13 20:54:17 +0800

    BINGO

---
+++ playground.js
@@ -0,0 +1,30 @@
+var list = new Array(75);
+
+for (var i = 0; i < 75; i++) {
+    list[i] = i + 1;
+}
+
+
+shuffleArray(list);
+
+
+for (var i = 0; i < 75; i++) {
+    console.log(list[i]);
+}
+
+
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+function shuffleArray(array) {
+
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = getRandomInt(i + 1);
+        temp = array[j];
+        array[j] = array[i];
+        array[i] = temp;
+    }
+
+}
